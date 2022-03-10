3662dba3bc4c00675f5156b339df85d6a3137d40
Christian Francini Mascagni <59649715+Chri2187@users.noreply.github.com>
2022-03-10 19:35:09 +0100

    Minor Update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Portfolio from "./components/Portfolio";
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
+      <Portfolio/>
+      <Footer/>
     </div>
   );
 }
